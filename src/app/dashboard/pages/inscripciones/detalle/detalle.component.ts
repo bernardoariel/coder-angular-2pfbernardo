d12c25de69e18a2234a41a2befcad551119c2fee
Ariel Bernardo <64559228+bernardoariel@users.noreply.github.com>
2023-04-28 11:52:39 -0300

    Agregando

--- src/app/dashboard/pages/inscripciones/detalle/detalle.component.ts
+++ src/app/dashboard/pages/inscripciones/detalle/detalle.component.ts
@@ -4,6 +4,11 @@ import { ListadoComponent } from '../listado/listado.component';
 import { Inscripcion } from 'src/app/core/services/inscripcion.service';
 import { CursoService } from 'src/app/core/services/curso.service';
 import { AlumnoService } from 'src/app/core/services/alumno.service';
+import { Estudiante } from 'src/app/core/interfaces/estudiante.interface';
+
+interface Data {
+  inscripcion: Inscripcion;
+}
 
 @Component({
   selector: 'app-detalle',
@@ -11,49 +16,56 @@ import { AlumnoService } from 'src/app/core/services/alumno.service';
   styleUrls: ['./detalle.component.scss']
 })
 export class DetalleComponent {
-  titulo:string = '';
-  tipo:string = '';
-  foto:string = '';
-  fotoDefault:string = '../assets/img/cursos/default.png'
-  idCurso:number = 0;
-  nombreCurso:string = '';
-  alumnos:string[]=[]
+
+  step = 0;
+
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
+
+  titulo = '';
+  tipo = '';
+  foto = '';
+  fotoDefault = '../assets/img/cursos/default.png';
+  idCurso = 0;
+  nombreCurso = '';
+  alumnos: Estudiante[] = [];
+
   constructor(
-    private dialogRef:MatDialogRef<ListadoComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: { inscripcion?: Inscripcion },
-    private cursoService:CursoService,
-    private alumnosService:AlumnoService
-  ){
-    if(data && data.inscripcion){
+    private dialogRef: MatDialogRef<ListadoComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Data,
+    private cursoService: CursoService,
+    private alumnosService: AlumnoService
+  ) {
+    if (data && data.inscripcion) {
       console.log('data.inscripcion::: ', data.inscripcion);
 
-      let fotoUrl = `../assets/img/cursos/${data.inscripcion.idCurso}.png`
+      const fotoUrl = `../assets/img/cursos/${data.inscripcion.idCurso}.png`;
 
-      this.foto = fotoUrl
-      this.titulo = data.inscripcion.nombre ;
-      // this.tipo = data.curso.tipo
-      this.foto =(data.inscripcion.id<4)?fotoUrl : this.fotoDefault
+      this.foto = fotoUrl;
+      this.titulo = data.inscripcion.nombre;
+      // this.tipo = data.curso.tipo;
+      this.foto = (data.inscripcion.idCurso < 4) ? fotoUrl : this.fotoDefault;
       console.log('data.inscripcion.idCurso::: ', data.inscripcion.idCurso);
       this.cursoService.getCursoById(data.inscripcion.idCurso).subscribe(
         curso => {
-          this.nombreCurso = curso?.nombre || 'No encontrado'
-
-        }
-      )
-      // Obtener los detalles de los alumnos inscritos en el curso
-      this.alumnosService.getAlumnos().subscribe(
-        alumnos => {
-          this.alumnos = data.inscripcion?.alumnosInscriptos?.map(
-            alumnoId => {
-              const alumno = alumnos.find(alumno => alumno.id === alumnoId);
-              return alumno ? alumno.nombre  : 'No encontrado';
-            }
-          ) || [];
+          this.nombreCurso = curso?.nombre || 'No encontrado';
         }
       );
-
-    } else {
-      this.alumnos = [];
+      // Obtener los detalles de los alumnos inscritos en el curso
+      if (data.inscripcion && data.inscripcion.alumnosInscriptos) {
+        this.alumnos = data.inscripcion.alumnosInscriptos.filter(alumno => typeof alumno === 'object') as Estudiante[];
+      } else {
+        this.alumnos = [];
+      }
     }
   }
 }
