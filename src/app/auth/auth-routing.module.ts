315a342f8c52275f840c98024351aa804bc562bb
Ariel Bernardo <64559228+bernardoariel@users.noreply.github.com>
2023-05-03 20:07:33 -0300

    quitando path del

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
+
 
 const routes: Routes = [
   {
@@ -10,12 +10,8 @@ const routes: Routes = [
       {
         path:'login',
         component:LoginComponent
-      },
-      {
-        path:'register',
-        component: RegisterComponent
       }
-
+     
     ]
   },
 ];
