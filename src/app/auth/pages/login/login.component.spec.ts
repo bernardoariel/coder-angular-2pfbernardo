e174df19f30c724ce78f3a384160ec73eb26a2c1
Ariel Bernardo <64559228+bernardoariel@users.noreply.github.com>
2023-06-05 12:36:45 -0300

    Agregando pruebas en login

--- src/app/auth/pages/login/login.component.spec.ts
+++ src/app/auth/pages/login/login.component.spec.ts
@@ -1,61 +1,82 @@
-import { AuthService } from 'src/app/core/services/auth.service';
-import { ReactiveFormsModule } from '@angular/forms';
+
 import { TestBed } from "@angular/core/testing";
 import { LoginComponent } from "./login.component";
-import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
+import { Store } from "@ngrx/store";
 import { RouterTestingModule } from "@angular/router/testing";
 import { MaterialModule } from "src/app/shared/material/material.module";
-import { PipesModule } from 'src/app/shared/pipes/pipes.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AuthServiceMock } from '../../mocks/auth-service.mock';
+import { ReactiveFormsModule } from "@angular/forms";
+import { PipesModule } from "src/app/shared/pipes/pipes.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AuthService } from "src/app/core/services/auth.service";
 
-describe('Pruebas de LoginComponente', ()=>{
+
+describe('Pruebas de LoginComponent', ()=>{
+  
   let component: LoginComponent;
-  beforeEach(async ()=>{
-    await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ],
-      imports:[
-        HttpClientTestingModule,
+
+  beforeEach( async ()=>{
+    TestBed.configureTestingModule({
+       declarations: [LoginComponent],
+       imports:[
+        HttpClientModule,
         RouterTestingModule,
         MaterialModule,
         ReactiveFormsModule,
         PipesModule,
         BrowserAnimationsModule
-      ],
-      providers:[
-        {
-          provide: AuthService,
-          useClass: AuthServiceMock
+       ],
+       providers: [
+      { provide: Store, useValue: {} } 
+    ],
+    }).compileComponents()
 
-        }
-      ]
-    })
-    .compileComponents();
     const fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
   })
 
   it('Si el campo email esta vacio el FormControl del email debe ser invalido', ()=>{
-    component.loginForm.setValue({email:'',password:''})
-
-    expect(component.emailControl.invalid).toBeTrue()
+     component.loginForm.setValue({email:null, password:null});
+     expect(component.emailControl.invalid).toBeTrue()
   })
-  it('Si el loginForm es invalido, debe marcar todos los controles como touched', () =>{
-    component.loginForm.setValue({email:'',password:''})
+  it('Si el campo password esta vacio el FormControl del email debe ser invalido', ()=>{
+     component.loginForm.setValue({email:null, password:null});
+     expect(component.passwordControl.invalid).toBeTrue()
+  })
+  
+  it('Si el loginForm es invalido, debe marcar todos los controles como touched',()=>{
+    component.loginForm.setValue({email:null, password:null});
+
     const spyOnMarkAllAsTouched = spyOn(component.loginForm, 'markAllAsTouched')
 
-    component.onSubmit()
-    /* expect(component.emailControl.touched).toBeTrue()
-    expect(component.passwordControl.touched).toBeTrue() */
-    expect(spyOnMarkAllAsTouched).toHaveBeenCalled();
-
-  })
-  it('si el login es valido debera llamar al login del authService', ()=>{
-    component.loginForm.setValue({email:'admin@admin.com',password:'123456'})
-    const spyOnLogin = spyOn(TestBed.inject(AuthService), 'login')
     component.onSubmit();
-    expect(component.loginForm.valid).toBeTrue()
-    expect(spyOnLogin).toHaveBeenCalled()
+
+    expect(spyOnMarkAllAsTouched).toHaveBeenCalled()
   })
+
+   it('Debe llamar a authService.login cuando loginForm es válido', () => {
+    const authService = TestBed.inject(AuthService);
+    const loginSpy = spyOn(authService, 'login').and.returnValue(Promise.resolve(true) as any);
+
+    component.loginForm.setValue({ email: 'admin@admin.com', password: '123456' });
+    component.onSubmit();
+
+    expect(loginSpy).toHaveBeenCalledWith({ email: 'admin@admin.com', password: '123456' });
+  });
+  
+  it('Debe establecer errorAutenticacion en verdadero si authService.login devuelve falso', async () => {
+    const authService = TestBed.inject(AuthService);
+    spyOn(authService, 'login').and.returnValue(Promise.resolve(false) as any);
+
+    component.loginForm.setValue({ email: 'admin@admin.com', password: '123456' });
+    await component.onSubmit();
+
+    expect(component.errorAutenticacion).toBeTrue();
+  });
+
+  
+
 })
